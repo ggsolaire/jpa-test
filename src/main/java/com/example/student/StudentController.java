f4122f2b251b736a597cf3207230cc484c3b32e2
ggsolaire <luigi.solaro2002@gmail.com>
2023-10-18 19:17:36 +0200

    Fixato il Mapping per i packages

--- src/main/java/com/example/student/StudentController.java
+++ src/main/java/com/example/student/StudentController.java
@@ -12,22 +12,24 @@ import java.util.List;
 
 //Questa notazione rende la classe capace di fornire dei Rest Endpoints
 @RestController
-@RequestMapping(path = "a")    //Questo path viene aggiunto a localhost:8080, quindi farà il mapping di questa classe
+@RequestMapping    //Questo path viene aggiunto a localhost:8080, quindi farà il mapping di questa classe
 public class StudentController {
 
 
     //Per fare in modo che questo metodo sia servito come Rest Endpoint, usiamo la notazione
 
-    @GetMapping
+    @GetMapping("/studentAPI")
     public List<Student> getStudent(){
         //JSON return List.of("Hello", "world!"); -> Ritorna un Array se si imposta tipo di ritorno a List<String>
         return List.of(new Student(
-
                 1L,
                 "Luigi",
                 LocalDate.of(2002, Month.MARCH, 31),
                 21
-        ));
+        ),
+                new Student(
+                        2L,"Marco",LocalDate.of(2003,Month.JANUARY,20),30
+                ));
     }
 //    Così facendo, la classe viene convertita in un JSON object
 
--- src/main/java/com/example/testjpa/TestJpaApplication.java
+++ src/main/java/com/example/testjpa/TestJpaApplication.java
@@ -2,8 +2,15 @@ package com.example.testjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+/*
+* Dice al programma
+*/
+@ComponentScan(basePackages = {"com.example.student", "com.example.testjpa"})
 public class TestJpaApplication {
 
     public static void main(String[] args) {
